09906875b3f0403a9a6c20b11ef4e3022a1fdfa6
nikhiljayant <njayant86@gmail.com>
2025-08-05 23:59:14 +0530

    pattern ques 16

--- pattern.js
+++ pattern.js
@@ -22,4 +22,15 @@ function pattern15(num) {
   console.log(str);
 }
 
-pattern15(5);
+function pattern16(num) {
+  let str = "";
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j <= i; j++) {
+      str += String.fromCharCode(65 + i);
+    }
+    str += "\n";
+  }
+  console.log(str);
+}
+
+pattern16(5);
