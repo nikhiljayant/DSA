a35d3a337bc3ded786304de39aeeef0db7ffb286
nikhiljayant <njayant86@gmail.com>
2025-08-17 20:54:24 +0530

    Add printN and printNBacktrack functions for number printing

--- recursion.js
+++ recursion.js
@@ -7,3 +7,32 @@ function printNameNtimes(name, times) {
 }
 
 // printNameNtimes("basic recursion", 5);
+
+function printN(n) {
+  let count = n;
+  if (count === 0) return;
+  console.log(count);
+  count--;
+  printN(count);
+}
+
+// printN(5);
+
+// Print from 1 to n using backtracking
+function printNBacktrack(n) {
+  let count = n;
+  if (count < 1) return;
+  printNBacktrack(count - 1);
+  console.log(count);
+}
+
+// printNBacktrack(3);
+
+// Print from n to 1 using backtracking
+function printNto1usingBacktrack(start, end) {
+  if (start === end) return;
+  printNto1usingBacktrack(start + 1, end);
+  console.log(start);
+}
+
+// printNto1usingBacktrack(1, 5);
