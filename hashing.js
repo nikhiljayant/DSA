6261a9bead1e8825a44d675e5a3183d56b1e41f9
nikhiljayant <njayant86@gmail.com>
2025-08-24 20:44:23 +0530

    Refactor hashing.js to add character hashing functions and comment out example calls

--- hashing.js
+++ hashing.js
@@ -7,4 +7,48 @@ function hashing(arr, noToFind) {
   console.log(hashArr[noToFind]);
 }
 
-hashing([1, 1, 1, 2, 3, 4, 5, 0, 0], 0);
+// hashing([1, 1, 1, 2, 3, 4, 5, 0, 0], 0);
+
+// CHARACTER HASHING 👇
+
+// For Lowercase Characters
+function lowercaseCharacterHashing(str = "") {
+  const hashArr = new Array(26).fill(0);
+
+  for (let i = 0; i < str.length; i++) {
+    hashArr[str.charCodeAt(i) - "a".charCodeAt(0)] += 1; // Using ASCII values ("a" is 97 in ASCII)
+  }
+
+  console.log(hashArr);
+}
+
+// lowercaseCharacterHashing("abdddd");
+
+// For All Characters
+function characterHashing(str = "") {
+  const hashArr = new Array(256).fill(0);
+
+  for (let i = 0; i < str.length; i++) {
+    hashArr[str.charCodeAt(i)] += 1;
+  }
+
+  console.log(hashArr);
+}
+
+// characterHashing("abddddABCD1234!@#");
+
+function hashingUsingMap(arr = []) {
+  const map = new Map();
+  for (let i = 0; i < arr.length; i++) {
+    if (map.has(arr[i])) {
+      map.set(arr[i], map.get(arr[i]) + 1);
+    } else {
+      map.set(arr[i], 1);
+    }
+  }
+  return map;
+}
+
+// console.log(hashingUsingMap([1, 1, 1, 2, 3, 4, 5, 0, 0, 12]).get(1));
+
+// hashingUsingMap([1, 1, 1, 2, 3, 4, 5, 0, 0, 12]);
