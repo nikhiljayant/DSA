3f249366facfadd316427e414ae909d438c6a923
nikhiljayant <njayant86@gmail.com>
2026-04-27 12:53:25 +0530

    mergeSortedArrays ques.

--- questions/array/medium.js
+++ questions/array/medium.js
@@ -1,3 +1,20 @@
+function mergeSortedArrays(nums1, m, nums2, n) {
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+
+  while (j >= 0) {
+    if (i >= 0 && nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
+  }
+}
+
 function mergeIntervals(nums) {
   if (nums.length <= 1) return nums;
 
