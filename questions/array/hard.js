3b7d1429d1636fb55fef4276fb745d1f27b6d76d
nikhiljayant <njayant86@gmail.com>
2026-04-27 12:54:37 +0530

    fourSum ques.

---
+++ questions/array/hard.js
@@ -0,0 +1,42 @@
+function fourSum(nums, target) {
+  if (nums.length < 4) return [];
+
+  nums.sort((a, b) => a - b);
+
+  const n = nums.length;
+
+  const result = [];
+
+  for (let i = 0; i < n - 3; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    for (let j = i + 1; j < n - 2; j++) {
+      if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+
+      let left = j + 1;
+      let right = n - 1;
+
+      while (left < right) {
+        const total = nums[i] + nums[j] + nums[left] + nums[right];
+
+        if (total === target) {
+          result.push([nums[i], nums[j], nums[left], nums[right]]);
+
+          left++;
+          right--;
+
+          while (left < right && nums[left] === nums[left - 1]) left++;
+          while (left < right && nums[right] === nums[right + 1]) right--;
+        } else if (total > target) {
+          right--;
+        } else {
+          left++;
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
+console.log(fourSum([1, 0, -1, 0, -2, 2], 0));
