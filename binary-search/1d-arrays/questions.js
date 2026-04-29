5a1567dd8051e3fffb6ae54c1b8668c20add5c76
nikhiljayant <njayant86@gmail.com>
2026-04-29 23:38:58 +0530

    add upperBound function to find the first element strictly greater than target

--- binary-search/1d-arrays/questions.js
+++ binary-search/1d-arrays/questions.js
@@ -20,3 +20,26 @@ function lowerBound(arr, target) {
 }
 
 console.log(lowerBound([1, 2, 4, 4, 5, 7], 4));
+
+// “First element strictly greater than target”
+function upperBound(arr, target) {
+  let low = 0;
+  let high = arr.length - 1;
+
+  let ans = arr.length;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (arr[mid] > target) {
+      ans = mid;
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return ans;
+}
+
+console.log(upperBound([1, 3, 3, 5, 8], 7));
