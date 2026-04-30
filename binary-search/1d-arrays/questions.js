fb0083014bc4fde462b43964b0564dfdbe5d4415
nikhiljayant <njayant86@gmail.com>
2026-04-30 22:24:59 +0530

    add searchInsertPosition function to determine the index for target insertion in a sorted array

--- binary-search/1d-arrays/questions.js
+++ binary-search/1d-arrays/questions.js
@@ -43,3 +43,25 @@ function upperBound(arr, target) {
 }
 
 console.log(upperBound([1, 3, 3, 5, 8], 7));
+
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+function searchInsertPosition(arr, target) {
+  let low = 0;
+  let high = arr.length - 1;
+
+  let ans = arr.length;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (arr[mid] >= target) {
+      ans = mid;
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return ans;
+}
