7738b95ca2c410e074b24783c8d59241cf25e424
nikhiljayant <njayant86@gmail.com>
2026-06-11 23:06:42 +0530

    feat: add solutions for common string manipulation algorithmic problems

--- questions/string/easy.js
+++ questions/string/easy.js
@@ -51,3 +51,20 @@ var isIsomorphic = function (s, t) {
 var rotateString = function (s, goal) {
     return s.length === goal.length && (s + s).includes(goal);
 };
+
+var isAnagram = function (s, t) {
+    if (s.length !== t.length) return false;
+
+    const count = new Array(26).fill(0);
+
+    for (let i = 0; i < s.length; i++) {
+        count[s.charCodeAt(i) - 97]++;
+        count[t.charCodeAt(i) - 97]--;
+    }
+
+    for (const num of count) {
+        if (num !== 0) return false;
+    }
+
+    return true;
+};
