5cc27e3c4dad8e313ef0d4657201be346eee604b
nikhiljayant <njayant86@gmail.com>
2026-06-17 22:48:43 +0530

    string maxDepth easy question

--- questions/string/easy.js
+++ questions/string/easy.js
@@ -140,3 +140,19 @@ var reverseWords = function (s) {
     return finalStr;
 
 };
+
+var maxDepth = function (s) {
+    let depth = 0;
+    let maxDepth = 0;
+
+    for (const char of s) {
+        if (char === "(") {
+            depth++;
+            maxDepth = Math.max(maxDepth, depth);
+        } else if (char === ")") {
+            depth--;
+        }
+    }
+
+    return maxDepth;
+};
